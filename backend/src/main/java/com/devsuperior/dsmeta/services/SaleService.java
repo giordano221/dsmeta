0b3f8bba2f44f09ffdfa85b855ca3f53a57bc468
= <=>
2022-07-16 01:42:23 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,11 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -17,6 +20,21 @@ public class SaleService
 	public List<Sale> findSales()
 	{
 		return repository.findAll();
+	}
+	
+	public Page<Sale> findSalesPageable(Pageable pageable)
+	{
+		return repository.findAll(pageable);
+	}
+	
+	public Page<Sale> findSalesPageableForDate( String minDate, String maxDate, Pageable pageable)
+	{
+		LocalDate today = LocalDate.now();
+		
+		LocalDate min = maxDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
 	}
 
 }
